3c467f4814dc98226ba1697c62acafe4cd32b286
chengyinok <chengyinok@qq.com>
2017-11-29 22:46:34 +0800

    Add 完善swagger构建restful api

--- config/src/main/java/com/study/swagger/SwaggerConfig.java
+++ config/src/main/java/com/study/swagger/SwaggerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -68,7 +69,8 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("包含媒体、咨询、搜索引擎关键字、广告等类型接口的服务")//大标题
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")//大标题
+                .contact(new Contact("sun","",""))
                 .version("1.0")//版本
                 .build();
     }
--- service/src/main/java/com/study/user/controller/UserController.java
+++ service/src/main/java/com/study/user/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.study.user.controller;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.study.dto.ResponseVo;
 import com.study.user.entity.User;
 import com.study.user.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,22 +21,58 @@ import org.springframework.web.bind.annotation.*;
  * @since 2017-11-28
  */
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user/{id}")
+    @ApiOperation(value="获取用户列表", notes="")
+    @GetMapping("")
+    public List<User> getUserList() {
+        List<User> r = userService.selectList(new EntityWrapper<User>());
+        return r;
+    }
+
+
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    @PostMapping("")
+    public ResponseVo postUser(User user){
+        ResponseVo responseVo = new ResponseVo();
+        boolean result = userService.insert(user);
+        responseVo.setSuccessful(result);
+        return responseVo;
+    }
+
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer",paramType = "path")
+    @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Integer id) {
         return userService.selectById(id);
     }
 
-    @PostMapping("/save")
-    public ResponseVo save(User user){
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer",paramType = "path"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
+    @PutMapping("/{id}")
+    public ResponseVo putUser(@PathVariable Integer id, @RequestBody User user) {
         ResponseVo responseVo = new ResponseVo();
-        boolean result = userService.insertOrUpdate(user);
-        responseVo.setSuccessful(result);
+        User u = userService.selectById(id);
+        u.setRealName(user.getRealName());
+        u.setAccountName(user.getAccountName());
+        responseVo.setSuccessful(userService.updateById(u));
+        return responseVo;
+    }
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Integer",paramType = "path")
+    @DeleteMapping("/{id}")
+    public ResponseVo deleteUser(@PathVariable Integer id) {
+        ResponseVo responseVo = new ResponseVo();
+        responseVo.setSuccessful(userService.deleteById(id));
         return responseVo;
     }
 }
