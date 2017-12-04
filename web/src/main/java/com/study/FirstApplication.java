220e0db94fb80eecb369e5dad4981847e284231c
chengyin <1034545813@qq.com>
2017-12-04 17:20:11 +0800

    Add Spring Boot增加监控功能。

--- web/src/main/java/com/study/FirstApplication.java
+++ web/src/main/java/com/study/FirstApplication.java
@@ -1,11 +1,15 @@
 package com.study;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class FirstApplication {
 
 	public static void main(String[] args) {
