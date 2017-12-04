22576ff2a5bb2ca5839ebbffe7b927a06eef095a
chengyinok <chengyinok@qq.com>
2017-12-04 21:59:16 +0800

    Add sba被监控配置。

--- web/src/main/java/com/study/FirstApplication.java
+++ web/src/main/java/com/study/FirstApplication.java
@@ -1,16 +1,26 @@
 package com.study;
 
-import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
-import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@EnablePrometheusEndpoint
-@EnableSpringBootMetricsCollector
-public class FirstApplication {
+public class FirstApplication extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FirstApplication.class);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class,args);
